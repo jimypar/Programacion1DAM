a6bcca789f2b246fd6254ab4cfb8dd75f789bc24
Jaime <jaime.pardo@fundacionmontessori.com>
2020-11-03 13:40:50 +0100

    Delimiter

--- Ejercicios2/Ej14_Menu_circulos/src/main/Main.java
+++ Ejercicios2/Ej14_Menu_circulos/src/main/Main.java
@@ -3,19 +3,17 @@ package main;
 import java.util.Scanner;
 
 public class Main {
-	
+
 	public static Scanner scan1 = new Scanner(System.in);
-	
-	
+
 	public static void main(String[] args) {
 
 		boolean restart = programa();
-		
-		
+
 		do {
-			
+
 			restart = programa();
-			
+
 		}
 
 		while (restart == true);
@@ -23,50 +21,49 @@ public class Main {
 	}
 
 	private static boolean programa() {
-		
+
 		try {
 			System.out.println("");
 			System.out.println("--------------------");
 			System.out.println("---MENU PRINCIPAL---");
 			System.out.println("1-Area del circulo ");
 			System.out.println("2-Volumen de la esfera");
+			System.out.println("3-Salir");
 
 			int num1 = scan1.nextInt();
-			
+
 			switch (num1) {
 
-			
 			case 1:
 				System.out.println("Introduce el radio de la circunferencia:");
 				double radio = scan1.nextDouble();
 				double area = Math.PI * Math.pow(radio, 2);
-				System.out.println("Area= "+area);
-				break;
+				System.out.println("Area= " + area);
+				return true;
 
 			case 2:
 				System.out.println("Introduce el radio de la circunferencia:");
 				double radio2 = scan1.nextDouble();
-				double vol = (4*Math.PI * Math.pow(radio2, 3))/3 ; 
-				System.out.println("Volumen= "+vol);
-				break;
+				double vol = (4 * Math.PI * Math.pow(radio2, 3)) / 3;
+				System.out.println("Volumen= " + vol);
+				return true;
+
+			case 3:
+				return false;
 
 			default:
 				System.out.println("Introduce un numero comprendido entre 1 y 2");
-			
+
 				return true;
 			}
 
 		} catch (Exception e) {
 
 			System.out.println("Introduce un numero entero");
-			
+
 			return true;
 
 		}
-
-		
-
-		return false;
 
 	}
 }
---
+++ Ejercicios3/Ej 2/src/main/Main.java
@@ -0,0 +1,63 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		try {
+
+			Scanner scan1 = new Scanner(System.in);
+
+			System.out.println("Introduce la hora");
+			System.out.println("hh:mm:ss");
+
+			String entrada = scan1.nextLine();
+
+			Scanner scan2 = new Scanner(entrada);
+
+			scan2.useDelimiter(":");
+
+			int h = Integer.parseInt(scan2.next());
+			int m = Integer.parseInt(scan2.next());
+			int s = Integer.parseInt(scan2.next());
+
+			if ((h < 24 && h >= 0) && (m < 60 && m >= 0) && (s < 60 && s >= 0)) {
+
+				s++;
+
+				if (s == 60) {
+
+					s = 0;
+					m++;
+
+				}
+
+				if (m == 60) {
+
+					m = 0;
+					h++;
+				}
+
+				if (h == 24) {
+
+					h = 0;
+				}
+
+			}
+
+			else {
+
+				System.out.println("Error");
+
+			}
+			System.out.println("Son las: " + h + ":" + m + ":" + s);
+			scan1.close();
+			scan2.close();
+		} catch (Exception e) {
+			System.out.println("Error");
+		}
+
+	}
+}
