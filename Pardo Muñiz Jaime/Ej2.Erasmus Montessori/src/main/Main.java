bdb89f7c35552015ad0519011c0ed3afcd03398a
Jaime <jaime.pardo@fundacionmontessori.com>
2020-11-18 08:37:45 +0100

    No se que ha ocurrido

---
+++ Pardo Muñiz Jaime/Ej2.Erasmus Montessori/src/main/Main.java
@@ -0,0 +1,103 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		try {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Elige (L)ondres, (P)raga o (B)rujas:");
+		String ciudad = scan.next().toLowerCase();
+		char city = ciudad.charAt(0);
+
+		System.out.println("Eres alumno del montessori? (True/False)");
+		boolean mont = scan.nextBoolean();
+
+		System.out.println("Introduce la nota:");
+		int nota = scan.nextInt();
+
+		if (city == 'l') {
+
+			if (nota >= 5 && mont == true) {
+
+				System.out.println("Puedes realizar el erasmus de Londres y te regalamos el curso.");
+
+			}
+
+			else if (nota >= 5 && mont == false) {
+
+				System.out.println("Puedes realizar el erasmus de Londres y no tienes regalo.");
+
+			}
+
+			else {
+
+				System.out.println("No puedes realizar el erasmus a londres");
+
+			}
+		}
+
+		if (city == 'p') {
+
+			if (nota >= 8 && mont == true) {
+
+				System.out.println("Puedes realizar el erasmus de Praga y te regalamos el viaje.");
+
+			}
+
+			else if ((nota < 8 && nota >= 5) && mont == true) {
+
+				System.out.println("Puedes realizar el erasmus de Praga y tienes descuento en el viaje");
+
+			}
+			
+			else if ((nota < 8 && nota >= 5) && mont == false) {
+
+				System.out.println("Puedes realizar el erasmus de Praga");
+
+			}
+
+			else {
+
+				System.out.println("No puedes realizar el erasmus a Praga");
+
+			}
+		}
+		
+		if (city == 'b') {
+
+			if (mont == true) {
+
+				System.out.println("Puedes realizar el erasmus de Bujas independientemente de la nota.");
+
+			}
+
+			else if (nota >= 5 && mont == false) {
+
+				System.out.println("Puedes realizar el erasmus de Brujas aunque no seas del colegio");
+
+			}
+			
+			else {
+
+				System.out.println("No puedes realizar el erasmus a Brujas");
+
+			}
+		}
+		
+		
+		scan.close();
+		
+		}
+		
+		catch(Exception e) {
+			
+			System.out.println("Algo ha fallado");
+		}
+	}
+
+}
