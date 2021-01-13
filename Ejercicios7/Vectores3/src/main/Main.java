ab85e7c08ff237f3d43170134671014c6640e9d3
Jaime <jaime.pardo@fundacionmontessori.com>
2021-01-13 19:48:44 +0100

    a

--- Ejercicios7/Vectores3/src/main/Main.java
+++ Ejercicios7/Vectores3/src/main/Main.java
@@ -4,42 +4,69 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static Scanner scan = new Scanner(System.in);
+	private static Scanner scan = new Scanner(System.in);
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        System.out.println("Cuanto numeros deseas introducir:");
-        int nums;
-        nums = scan.nextInt();
+		System.out.println("Cuanto numeros deseas introducir:");
+		int nums;
+		nums = scan.nextInt();
 
-        int[] lista = RellenarArray(nums);
+		int[] lista = RellenarArray(nums);
 
-        VisualizarArray(nums, lista);
-        scan.close();
-    }
+		VisualizarArray(nums, lista);
+		VisualizarRepeat(nums, lista);
+		scan.close();
+	}
 
-    private static int[] RellenarArray(int nums) {
+	private static int[] RellenarArray(int nums) {
 
-        int[] lista = new int[nums];
+		int[] lista = new int[nums];
 
-        for (int i = 0; i < nums; i++) {
+		for (int i = 0; i < nums; i++) {
 
-            System.out.print("Introduce un numero:");
-            lista[i] = scan.nextInt();
+			System.out.print("Introduce un numero:");
+			lista[i] = scan.nextInt();
 
-        }
+		}
 
-        return lista;
+		return lista;
 
-    }
+	}
 
-    private static void VisualizarArray(int nums, int[] lista) {
+	private static void VisualizarArray(int nums, int[] lista) {
 
-        System.out.print("Lista= ");
+		System.out.print("Lista= ");
 
-        for (int a = 0; a < nums; a++) {
-            System.out.print(lista[a] + " ");
-        }
-    }
+		for (int a = 0; a < nums; a++) {
+			System.out.print(lista[a] + " ");
+		}
+
+		System.out.println();
+
+	}
+
+	private static void VisualizarRepeat(int nums, int[] lista) {
+
+		System.out.print("Repetidos= ");
+		
+		int[] Repetidos = new int[nums];
+
+
+		for (int x = 0; x < (lista.length+1); x++) {
+
+			for (int y = 1; y < (lista.length+1); y++) {
+
+				if (lista[x] == lista[y]) {
+					
+					System.out.print(lista[x]);
+
+				}
+
+			}
+
+		}
+
+	}
 
 }
