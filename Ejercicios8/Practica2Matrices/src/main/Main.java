0a4017c6b73669e968caabb34b770c64029279e9
Jaime <jaime.pardo@fundacionmontessori.com>
2021-02-02 15:18:45 +0100

    Matrices

---
+++ Ejercicios8/Practica2Matrices/src/main/Main.java
@@ -0,0 +1,55 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int rango = 0;
+		boolean error = false;
+		do {
+
+			try {
+				System.out.print("Introduce el rango: ");
+				rango = Integer.parseInt(in.readLine());
+				error = false;
+			} catch (NumberFormatException e) {
+				System.out.println("Numero incorrecto");
+				error = true;
+			} catch (IOException e) {
+				System.out.println("ERROR");
+				error = true;
+			}
+		} while (error);
+
+		int matriz[][] = new int[rango][rango];
+
+		
+		try {
+			OperacionesMatriz.rellenar(matriz);
+		} catch (NumberFormatException | IOException e) {
+			System.out.println("Error");
+			e.printStackTrace();
+		}
+		OperacionesMatriz.visualizar(matriz);
+		if (OperacionesMatriz.simetrica(matriz)) {
+			System.out.println();
+			System.out.println("Es simetrica");
+		}
+		else {
+			System.out.println();
+			System.out.println("No es simetrica");
+		}
+
+		OperacionesMatriz.MaxFila();
+		OperacionesMatriz.MaxColumna();
+		OperacionesMatriz.MaxMatriz();
+		OperacionesMatriz.SumaFilas();
+		OperacionesMatriz.SumaColumnas();
+		
+	}
+
+}
---
+++ Ejercicios8/Practica2Matrices/src/main/OperacionesMatriz.java
@@ -0,0 +1,74 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class OperacionesMatriz {
+
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void rellenar(int[][] m) throws NumberFormatException, IOException {
+
+		for (int x = 0; x < m.length; x++) {
+			System.out.println("Fila: " + (x + 1));
+
+			for (int i = 0; i < m[0].length; i++) {
+				System.out.print("Columna " + (i + 1) + ": ");
+				m[x][i] = Integer.parseInt(in.readLine());
+			}
+		}
+
+	}
+
+	public static void visualizar(int[][] m) {
+
+		for (int x = 0; x < m.length; x++) {
+			System.out.println();
+			for (int i = 0; i < m[0].length; i++) {
+				System.out.print(m[x][i] + " ");
+			}
+		}
+
+	}
+
+	public static boolean simetrica(int[][] m) {
+
+		for (int i = 0; i < m.length; i++) {
+			for (int x = 0; x < m.length; x++) {
+				if (m[i][x] != m[x][i]) {
+					return false;
+				}
+			}
+
+		}
+		return true;
+	}
+
+	public static void MaxFila() {
+		
+		
+		
+	}
+
+	public static void MaxColumna() {
+		
+		
+	}
+
+	public static void MaxMatriz() {
+		
+		
+	}
+
+	public static void SumaFilas() {
+		
+		
+	}
+
+	public static void SumaColumnas() {
+		
+		
+	}
+
+}
