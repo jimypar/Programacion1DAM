f4e7372564adf06c259ff19615bf3599e24eb110
jaime <jaime.pardo@fundacionmontessori.com>
2021-02-16 18:25:30 +0100

    Barcos Terminado

--- Ejercicios8/Juego1/src/main/Main.java
+++ Ejercicios8/Juego1/src/main/Main.java
@@ -3,21 +3,59 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Tablero tablero1 = new Tablero();
-		Tablero posicion1 = new Tablero();
-		
-		tablero1.visualizar(tablero1.getTablero1());
+
+		Tablero jugador1 = new Tablero();
+		Tablero jugador2 = new Tablero();
+
+		// JUGADOR 1
+		System.out.println("JUGADOR 1:");
+		jugador1.visualizar(jugador1.getTablero1());
 		do {
-		tablero1.vaciarTablero();
-		tablero1.introducirBarco(tablero1.getTablero1());
-		tablero1.visualizar(tablero1.getTablero1());
-		}while(tablero1.repetirBarco());
-		
+			jugador1.vaciarTableros();
+			jugador1.introducirBarco(jugador1.getTablero1());
+			jugador1.visualizar(jugador1.getTablero1());
+		} while (jugador1.repetirBarco());
+
+		// JUGADOR 2
+		System.out.println("JUGADOR 2:");
+		jugador2.visualizar(jugador2.getTablero1());
 		do {
-		tablero1.ataque();	
-		}while();
-		
+			jugador2.vaciarTableros();
+			jugador2.introducirBarco(jugador2.getTablero1());
+			jugador2.visualizar(jugador2.getTablero1());
+		} while (jugador2.repetirBarco());
+
+		do {
+			// turno jugador1
+			do {
+				System.out.println("JUGADOR 1:");
+				jugador1.visualizar(jugador1.getTablero2());
+				jugador1.ataque(jugador2.getTablero1(), jugador1.getTablero2());
+			} while (jugador1.getAcierto() && !jugador1.ganado());
+			// turno jugador2
+			do {
+				System.out.println("JUGADOR 2:");
+				jugador2.visualizar(jugador2.getTablero2());
+				jugador2.ataque(jugador1.getTablero1(), jugador2.getTablero2());
+			} while (jugador2.getAcierto() && !jugador2.ganado());
+		} while (!jugador1.ganado() && !jugador2.ganado());
+
+		// FIN
+		if (jugador1.ganado() && jugador2.ganado()) {
+			System.out.println("SE HA EMPATADO");
+		} else {
+			if (jugador1.ganado()) {
+				System.out.println("JUGADOR 1:");
+				jugador1.visualizar(jugador1.getTablero2());
+				System.out.println("EL JUGADOR 1 HA GANADO");
+			}
+			if (jugador2.ganado()) {
+				System.out.println("JUGADOR 2:");
+				jugador2.visualizar(jugador2.getTablero2());
+				System.out.println("EL JUGADOR 2 HA GANADO");
+			}
+		}
+
 	}
 
 }
--- Ejercicios8/Juego1/src/main/Tablero.java
+++ Ejercicios8/Juego1/src/main/Tablero.java
@@ -7,30 +7,48 @@ public class Tablero {
 	private Scanner scan = new Scanner(System.in);
 
 	private String tablero1[][] = new String[10][10];
-	private String posicion[][] = new String[10][10];
+	private String tablero2[][] = new String[10][10];
+	private int barcoundido;
+	private int intento;
+	private boolean acierto;
 
+	// CONSTRUCTOR TABLERO
 	Tablero() {
 		for (int x = 0; x < this.tablero1.length; x++) {
 			for (int i = 0; i < this.tablero1[0].length; i++) {
 				this.tablero1[x][i] = " ";
-				this.posicion[x][i] = " ";
+				this.tablero2[x][i] = " ";
 			}
 		}
+		barcoundido = 0;
+		intento=1;
+		acierto = false;
+	}
+
+	// GETTERS Y SETTERS
+	public int getBarcoundido() {
+		return barcoundido;
+	}
+
+	public void setBarcoundido(int barcoundido) {
+		this.barcoundido = barcoundido;
 	}
 
 	public String[][] getTablero1() {
 		return tablero1;
 	}
-	public void setTablero1(String[][] tablero1) {
-		this.tablero1 = tablero1;
+
+	public String[][] getTablero2() {
+		return tablero2;
 	}
-	public String[][] getPosicion() {
-		return posicion;
+	public boolean getAcierto() {
+		return acierto;
 	}
-	public void setPosicion(String[][] posicion) {
-		this.posicion = posicion;
+	public void setAcierto(boolean acierto) {
+		this.acierto = acierto;
 	}
 
+	// VISUALIZAR TABLA
 	public void visualizar(String[][] m) {
 		System.out.println("Y-> 0   1   2   3   4   5   6   7   8   9   ");
 		for (int x = 0; x < m.length; x++) {
@@ -50,7 +68,7 @@ public class Tablero {
 
 		for (int i = 0; i < 3; i++) {
 			do {
-				System.out.println("Introduce el las cordenadas x/z (Barco " + (i+1) + ")");
+				System.out.println("Introduce las cordenadas x/y (Barco " + (i + 1) + ")");
 				System.out.print("X:");
 				x = scan.nextInt();
 				System.out.print("Y:");
@@ -62,34 +80,61 @@ public class Tablero {
 
 	}
 
-	public void vaciarTablero() {
+	public void vaciarTableros() {
 		for (int x = 0; x < this.tablero1.length; x++) {
 			for (int i = 0; i < this.tablero1[0].length; i++) {
 				this.tablero1[x][i] = " ";
-				this.posicion[x][i] = " ";
+				this.tablero2[x][i] = " ";
 			}
 		}
 	}
-	
+
 	public boolean repetirBarco() {
 		String respuesta = "";
 		do {
-		System.out.println("Estan bien asi los barcos? (si/no)");
-		respuesta = scan.next();
-		if(respuesta.trim().toLowerCase().equalsIgnoreCase("si")) {
-			return false;
-		}
-		if(respuesta.trim().toLowerCase().equalsIgnoreCase("no")) {
-			return true;
-		}
-		}while(!respuesta.trim().toLowerCase().equalsIgnoreCase("no") || !respuesta.trim().toLowerCase().equalsIgnoreCase("si"));
+			System.out.println("Estan bien asi los barcos? (si/no)");
+			respuesta = scan.next();
+			if (respuesta.trim().toLowerCase().equalsIgnoreCase("si")) {
+				return false;
+			}
+			if (respuesta.trim().toLowerCase().equalsIgnoreCase("no")) {
+				return true;
+			}
+		} while (!respuesta.trim().toLowerCase().equalsIgnoreCase("no")
+				|| !respuesta.trim().toLowerCase().equalsIgnoreCase("si"));
 		return false;
 	}
-	
-	public void ataque() {
+
+	public void ataque(String[][] tab1, String[][] tab2) {
+		int x = 0;
+		int y = 0;
+
+		do {
+			System.out.println("Introduce las cordenadas x/y (Intento: "+this.intento+")");
+			System.out.print("X:");
+			x = scan.nextInt();
+			System.out.print("Y:");
+			y = scan.nextInt();
+		} while ((x < 0 || x > 9) && (y < 0 || y > 9));
+		if (tab1[x][y].contentEquals("O")) {
+			tab2[x][y] = "X";
+			this.barcoundido++;
+			this.acierto = true;
+		} else if (tab1[x][y].contentEquals(" ")) {
+			tab2[x][y] = "o";
+			this.acierto = false;
+		}
+		this.intento++;
 		
 	}
+
+	public boolean ganado() {
+		if (this.barcoundido == 3) {
+			return true;
+		}
+		return false;
+	}
+
 	
-	public boolean 
 
 }
